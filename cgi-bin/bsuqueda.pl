933b5424d264291cac21c1cc07f68cdf538f58e3
Gonzalo01703 <glaymem@unsa.edu.pe>
2022-11-19 15:04:56 -0500

    Se implemeto parte del archivo perl

---
+++ cgi-bin/bsuqueda.pl
@@ -0,0 +1,18 @@
+#!"c:\xampp\perl\bin\perl.exe"
+use strict;
+use warnings;
+use CGI;
+
+
+print "Content-type: text/html\n\n";
+print <<HTML;
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <link rel="stylesheet" type="text/css" href="../estilos.css">
+    <title>Busquedas bibliograficas de Programacion Web 1 </title>
+</head>
+<body>
+    <div >
+HTML
