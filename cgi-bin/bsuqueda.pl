38faff671dba8fd1c05988b0f4d4b2f597de9047
Gonzalo01703 <glaymem@unsa.edu.pe>
2022-11-20 22:57:58 -0500

    se cambio para que solo aparescarn las 4 que piden

--- cgi-bin/bsuqueda.pl
+++ cgi-bin/bsuqueda.pl
@@ -1,99 +1,96 @@
-#!"c:\xampp\perl\bin\perl.exe"
+#!C:\xampp\perl\bin\perl.exe
 use strict;
 use warnings;
+
 use CGI;
+my $q= CGI->new;
+print $q->header('text/html','utf-8');
+my $tipo= $q->param("kind");
+my $valor= $q->param("keyword");
+$valor = uc($valor); 
+
+my $exp = buildRegex(22);
 
 
-print "Content-type: text/html\n\n";
-print <<HTML;
+my @universidades = buscador($tipo,$valor); 
+
+my @campos = ("nombre","periodo","departamento","denominacion");
+
+my $table = renderTable();
+renderHTML("estilo.css",$table);
+
+sub renderTable{
+  $table = "<table border='2'>\n";
+  $table.= "<tr>\n";
+  foreach my $aux(@campos){
+    $table.= "<th>".$aux."</th>\n";
+  }
+  $table.= "</tr>\n";
+  foreach my $uni (@universidades){
+      $table.= "<tr>\n";
+      foreach my $campo (@campos){
+          $table.= "<td>".%{$uni}{$campo}."</td>\n";  
+      }
+      $table.= "</tr>\n";
+  }
+  $table.= "</table>\n";
+}
+sub renderHTML{
+  my $css = $_[0];
+  my $body = $_[1];
+  print <<BLOCK;
 <!DOCTYPE html>
 <html>
 <head>
-    
-    <link rel="stylesheet" type="text/css" href="../estilo2.css">
-    <title>buscando</title>
+  <meta charset="utf-8"> 
+  <link rel="stylesheet" type="text/css" href="estilo.css">
+  <title>Consultas  </title>
 </head>
 <body>
-    <div >
-HTML
+$body
+</body>
+</html>
+BLOCK
 
-my $q = CGI->new;
-my $busqueda= $q->param("busqueda");
-my $indicador = $q->param("indicador");
-my $flag;
-my $contador=0;
-my $encontrados=0;
-
-    $busqueda = uc $busqueda;
-    print "USTED INGRESO: ".$busqueda;
-
-    if($indicador eq "Nombre De Universidad"){
-        &busqueda(1);
-    } 
-    elsif($indicador eq "Periodo De Licenciamiento"){
-        &busqueda(4);
-    }elsif($indicador eq "Departamento Local"){
-        &busqueda(10);
-    }elsif($indicador eq "Denominacion De Programa"){
-        &busqueda(16);
-    }else{
-        print "ERROR";
-    }
-
-if(!defined($flag)){
-    print "<h1>No encontrado</h1>\n";
 }
 
-print <<HTML;
- Ingrese <a href="../index.html">aqui</a> para regresar al formulario de búsqueda
- </body>
-</html>
-HTML
+sub buscador{
+  my $kind = $_[0];
+  my $keyword = $_[1];
+  my @universidades=();
+  
+  open(IN, "../Programas.csv" ) or die("Error al abrir el archivo");
+  my @registros = <IN>;
+  close(IN);
 
-sub busqueda{
-    my ($numCasillero)=@_;
-    open(IN,"../licenciadas.csv") or die "<h1>ERROR: open file</h1>\n";
-        while(my $line = <IN>){
-            my @palabras= split("/",$line);
-    
-            if($contador==0){
-                print <<HTML;
-                <div class="forma">
-HTML
-                foreach my $element (@palabras){
-                    
-                    print <<HTML;
-                    <div>
-HTML
-                    print $element;
-                    print <<HTML;
-                    </div>
-HTML
-                }
-                print <<HTML;
-                    </div>
-HTML
-                $contador++;
-            }
-            if($palabras[$numCasillero]=~ /$busqueda/){
-                 print <<HTML;
-                <div class="forma">
-HTML
-
-                foreach my $element (@palabras){
-                    print <<HTML;
-                    <div>
-HTML
-                    print $element;
-                    print <<HTML;
-                    </div>
-HTML
-                }
-                print <<HTML;
-                    </div>
-HTML
-                $flag=1;
-            }
+  my $cont=0;
+  my $universidad;
+  for(my $i=0;$i<@registros;$i++){
+    if($registros[$i] =~ /$exp/){
+      if(defined($2) && defined($5)&& defined($11)&& defined($17)){
+        $universidad ={
+          "nombre" => $2,
+          "periodo" => $5,
+          "departamento" => $11,
+          "denominacion" => $17,
+        };
+        if(%{$universidad}{$kind}=~/.*$keyword.*/){
+          push @universidades,$universidad;
         }
-    close(IN);
+      }
+    } 
+  }
+  return @universidades;
+}
+
+sub buildRegex{
+  my $n = $_[0];
+  my $general='([^\|]+)\|';
+  my $ultimo = '([^\|]+)';
+ 
+  my $str = '^';
+  for(my $i=0;$i<$n;$i++){
+    $str.=$general;
+  }
+  return $str.$ultimo;
 }
